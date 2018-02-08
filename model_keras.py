6cf1c53aaa569101d88e0b0b96cffa05f8865b01
SunQpark <SunQ0313@gmail.com>
2018-02-08 19:41:57 +0900

    corrected strides

--- model_keras.py
+++ model_keras.py
@@ -32,22 +32,22 @@ def model(X_input, y):
     X = Conv2D(1024, (3, 3), activation='relu')(X)
     X = Conv2D(1024, (3, 3), activation='relu')(X)
 
-    X = Conv2DTranspose(512, (2, 2))(X)
+    X = Conv2DTranspose(512, (2, 2), strides=(2, 2))(X)
     X = concatenate([X3, X])
     X = Conv2D(512, (3, 3), activation='relu')(X)
     X = Conv2D(512, (3, 3), activation='relu')(X)
 
-    X = Conv2DTranspose(256, (2, 2))(X)
+    X = Conv2DTranspose(256, (2, 2), strides=(2, 2))(X)
     X = concatenate([X2, X])
     X = Conv2D(256, (3, 3), activation='relu')(X)
     X = Conv2D(256, (3, 3), activation='relu')(X)
 
-    X = Conv2DTranspose(128, (2, 2))(X)
+    X = Conv2DTranspose(128, (2, 2), strides=(2, 2))(X)
     X = concatenate([X1, X])
     X = Conv2D(128, (3, 3), activation='relu')(X)
     X = Conv2D(128, (3, 3), activation='relu')(X)
         
-    X = Conv2DTranspose(64, (2, 2))(X)
+    X = Conv2DTranspose(64, (2, 2), strides=(2, 2))(X)
     X = concatenate([X0, X])
     X = Conv2D(64, (3, 3), activation='relu')(X)
     X = Conv2D(64, (3, 3), activation='relu')(X)

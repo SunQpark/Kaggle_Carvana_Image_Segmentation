3c35d14308172f207e8cc06eb857cead57290f0c
SunQpark <SunQ0313@gmail.com>
2018-02-10 01:45:22 +0900

    deleted old codes

--- train_keras.py
+++ train_keras.py
@@ -16,20 +16,3 @@ if __name__ == '__main__':
     verbose=1)
 
     model.save('models/model_180209.hdf5')
-
-    # file_list = [filename.replace('.jpg', '') for filename in os.listdir('inputs/train/')] 
-
-    # images = []
-    # labels = []
-
-    # for filename in file_list:
-    #     img = np.array(Image.open("inputs/train/{}.jpg".format(filename)))
-    #     mask = np.array(Image.open("inputs/train_masks/{}_mask.gif".format(filename)))
-        
-    #     images.append(img)
-    #     labels.append(mask)
-
-    # images = np.array(images)/255
-    # labels = np.array(labels)[:, :, :, np.newaxis]
-    # labels = np.pad(labels, ((0, 0), (2, 2), (3, 3), (0, 0)), mode='constant', constant_values=0)
-    # print(labels.shape)

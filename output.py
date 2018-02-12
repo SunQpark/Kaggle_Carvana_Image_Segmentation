119fd024ab61e40834cf05346e1bf054a53d8070
SunQpark <SunQ0313@gmail.com>
2018-02-12 20:48:27 +0900

    model file, test code

--- output.py
+++ output.py
@@ -5,18 +5,21 @@ import numpy as np
 from PIL import Image
 
 filename = '0cdf5b5d0ce1_05'
+model_name = 'model_180212'
+out_dir = 'outputs/'
 
 img = np.array(Image.open("inputs/train/original/{}.jpg".format(filename)))
-# label_orig = np.array(Image.open("inputs/train_mask/original/{}_mask.gif".format(filename)))
+label_true = np.array(Image.open("inputs/train_mask/original/{}_mask.gif".format(filename)))
 
-model = load_model('models/model_180212.hdf5', 
+model = load_model('models/{}.hdf5'.format(model_name), 
     custom_objects={'dice_with_l2_loss':dice_with_l2_loss, 'dice_coef':dice_coef})
     
 img = img[np.newaxis, 0:1268, 0:1908, :]
 label_result = model.predict(img)
 label_result = np.squeeze(label_result)
 # np.savetxt("hi.csv", label_result, delimiter=",")
-
-print(label_result.shape)
-plt.imshow(label_result)
-plt.show()
+label_pred = Image.fromarray(label_result)
+label_pred.save(out_dir + filename + '_out.png')
+# print(label_pred.shape)
+# plt.imshow(label_pred)
+# plt.show()

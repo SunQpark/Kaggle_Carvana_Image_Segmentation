4f763817c455b19c3ab65db003ebfced97c74f1b
SunQpark <SunQ0313@gmail.com>
2018-02-12 18:05:22 +0900

    doing output test

--- output.py
+++ output.py
@@ -4,19 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-filename = '0cdf5b5d0ce1_01'
+filename = '0cdf5b5d0ce1_05'
 
 img = np.array(Image.open("inputs/train/original/{}.jpg".format(filename)))
-label = np.array(Image.open("inputs/train_mask/original/{}_mask.gif".format(filename)))
-
-plt.imshow(img)
-plt.show()
-
-plt.imshow(label)
-plt.show()
+# label_orig = np.array(Image.open("inputs/train_mask/original/{}_mask.gif".format(filename)))
 
 model = load_model('models/model_180212.hdf5', 
     custom_objects={'dice_with_l2_loss':dice_with_l2_loss, 'dice_coef':dice_coef})
+    
 img = img[np.newaxis, 0:1268, 0:1908, :]
 label_result = model.predict(img)
 label_result = np.squeeze(label_result)

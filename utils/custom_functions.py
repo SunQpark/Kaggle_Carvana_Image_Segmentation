1dbd832d237477267e400b847156bb319b981b97
SunQpark <SunQ0313@gmail.com>
2018-02-13 16:03:38 +0900

    weighted sum on custom loss function

--- utils/custom_functions.py
+++ utils/custom_functions.py
@@ -10,7 +10,8 @@ def dice_coef(y_true, y_pred, smooth=1):
     union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3])
     return K.mean((2. * intersection + smooth) / (union + smooth), axis=0)
 
-def dice_with_l2_loss(y_true, y_pred):
+def dice_with_l2_loss(y_true, y_pred, weight_l2=1.0):
     l2_loss = mean_squared_error(y_true, y_pred)
     dice_loss = 1 - dice_coef(y_true, y_pred)
-    return l2_loss + dice_loss
+    total_loss = weight_l2 * l2_loss + (1.0 - weight_l2)*dice_loss
+    return total_loss

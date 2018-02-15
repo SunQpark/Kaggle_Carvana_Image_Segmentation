c8bd18f0f381a994c47b84e1c0331b403061f55f
SunQpark <SunQ0313@gmail.com>
2018-02-15 21:47:22 +0900

    crossentropy with l2 loss

--- utils/custom_functions.py
+++ utils/custom_functions.py
@@ -1,9 +1,11 @@
 import keras.backend as K
-from keras.losses import mean_squared_error
 
 def custom_objects():
-    return {'dice_coef':dice_coef, 'dice_with_l2_loss':dice_with_l2_loss}
-
+    return {
+        'dice_coef':dice_coef, 
+        'dice_with_l2_loss':dice_with_l2_loss, 
+        'crossentropy_with_l2':crossentropy_with_l2
+        }
 
 def dice_coef(y_true, y_pred, smooth=1):
     intersection = K.sum(y_true * y_pred, axis=[1,2,3])
@@ -11,7 +13,11 @@ def dice_coef(y_true, y_pred, smooth=1):
     return K.mean((2. * intersection + smooth) / (union + smooth), axis=0)
 
 def dice_with_l2_loss(y_true, y_pred, weight_l2=1.0):
-    l2_loss = mean_squared_error(y_true, y_pred)
+    l2_loss = K.losses.mean_squared_error(y_true, y_pred)
     dice_loss = 1 - dice_coef(y_true, y_pred)
     total_loss = weight_l2 * l2_loss + (1.0 - weight_l2)*dice_loss
     return total_loss
+
+def crossentropy_with_l2(y_true, y_pred, weight_l2=10.0):
+    crossentropy = K.binary_crossentropy(y_true, y_pred)
+    l2 = K.losses.mean_squared_error(y_true, y_pred)

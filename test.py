58386586b5715d89551d61b23f5b66da8b6b911e
SunQpark <SunQ0313@gmail.com>
2018-02-17 14:50:28 +0900

    test.py modified

--- test.py
+++ test.py
@@ -9,53 +9,45 @@ import csv
 from PIL import Image
 
 
-def batch_id_gen(batch_size=2):
+def batch_id_generator(batch_size=4):
     id_list = [id.replace('.jpg', '') for id in os.listdir('inputs/test')]
+    num_id = len(id_list)
+    num_loops = int(num_id/batch_size)
     iterator = 0
-    while iterator + batch_size < len(id_list):
-        end_point = min([iterator + batch_size, len(id_list)])
-        id_batch = id_list[iterator:end_point]
-        yield id_batch
-        iterator += batch_size
-
-for i in range(3):
-    id_gen = batch_id_gen()
-    print(next(id_gen))
-
-# model_name = 'Unet_180216_samepad'
-# out_dir = 'outputs/'
-
-# num_total = len(id_list)
-
-
-# id_batch = 
-# img_size = (1280, 1918,)
-# img_batch = np.empty()
-
-
-# with open('test_mask.csv', 'w', newline='') as csvfile:
-#     write_model = csv.writer(csvfile)
-#     write_model.writerow(["img", "rle_mask"])
-
-#     for x, filename in enumerate(id_list):
-#         img_path = "inputs/test/{}.jpg".format(filename)
-#         img = np.array(Image.open(img_path))
-#         img = img/255
-#         img = img[np.newaxis, 0:1280, 0:1918, :]
-
-#         model = load_model('models/{}.hdf5'.format(model_name), custom_objects=custom_objects())
-
-#         label_result = model.predict(img)
-#         label_result = np.squeeze(label_result)
-
-#         for i in range(1280):
-#             for j in range(1918):
-#                 if label_result[i, j] > 0.5:
-#                     label_result[i, j] = 1
-#                 else:
-#                     label_result[i,j] = 0
+    for i in range(num_loops+1):
+        start = batch_size * i
+        end = min([batch_size * (i + 1), num_id])
         
-#         label_result = label_result.astype(np.uint8)
+        id_batch = id_list[start:end]
+        yield id_batch
 
-#         write_model.writerow([filename, rle_encode(label_result)])
-#         print(filename, 'has completed (',x+1, 'out of', num_total, ')')
+if __name__ == '__main__':
+    model_name = 'Unet_180216_samepad'
+    out_dir = 'outputs/'
+    img_size = (1280, 1918,)
+    model = load_model('models/{}.hdf5'.format(model_name), custom_objects=custom_objects())
+
+    with open(out_dir + 'submission.csv', 'w', newline='') as csv_file:
+        write_model = csv.writer(csv_file)
+        write_model.writerow(["img", "rle_mask"])
+
+        id_gen = batch_id_generator()
+        while True:
+            try:
+                id_batch = next(id_gen)
+            except(StopIteration):
+                print('Done.')
+                break
+
+            images = np.empty((len(id_batch),) + img_size + (3,))
+            for i, filename in enumerate(id_batch):
+                img_path = "inputs/test/{}.jpg".format(filename)
+                img = np.array(Image.open(img_path))
+                img = img/255
+                images[i, :, :, :] = img[0:1280, 0:1918, :]
+            
+            label_result = (model.predict(images) > 0.5)
+
+            for i, filename in enumerate(id_batch):
+                rle_mask = rle_encode(label_result[i, :, :, 0])
+                write_model.writerow([filename + '.jpg', rle_mask])

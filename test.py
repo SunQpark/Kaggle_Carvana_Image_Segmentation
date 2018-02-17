477131fc3b15b319564246df58b8786f74c6aa8e
SunQpark <SunQ0313@gmail.com>
2018-02-17 14:10:03 +0900

    using batch in test

--- test.py
+++ test.py
@@ -9,36 +9,53 @@ import csv
 from PIL import Image
 
 
-model_name = 'Unet_180216_samepad'
-out_dir = 'outputs/'
+def batch_id_gen(batch_size=2):
+    id_list = [id.replace('.jpg', '') for id in os.listdir('inputs/test')]
+    iterator = 0
+    while iterator + batch_size < len(id_list):
+        end_point = min([iterator + batch_size, len(id_list)])
+        id_batch = id_list[iterator:end_point]
+        yield id_batch
+        iterator += batch_size
 
-id_list = [id.replace('.jpg', '') for id in os.listdir('inputs/test')]
-total = len(id_list)
+for i in range(3):
+    id_gen = batch_id_gen()
+    print(next(id_gen))
+
+# model_name = 'Unet_180216_samepad'
+# out_dir = 'outputs/'
+
+# num_total = len(id_list)
 
 
-with open('test_mask.csv', 'w', newline='') as csvfile:
-    write_model = csv.writer(csvfile)
-    write_model.writerow(["img", "rle_mask"])
+# id_batch = 
+# img_size = (1280, 1918,)
+# img_batch = np.empty()
 
-    for x, filename in enumerate(id_list):
-        img_path = "inputs/test/{}.jpg".format(filename)
-        img = np.array(Image.open(img_path))
-        img = img/255
-        img = img[np.newaxis, 0:1280, 0:1918, :]
 
-        model = load_model('models/{}.hdf5'.format(model_name), custom_objects=custom_objects())
+# with open('test_mask.csv', 'w', newline='') as csvfile:
+#     write_model = csv.writer(csvfile)
+#     write_model.writerow(["img", "rle_mask"])
 
-        label_result = model.predict(img)
-        label_result = np.squeeze(label_result)
+#     for x, filename in enumerate(id_list):
+#         img_path = "inputs/test/{}.jpg".format(filename)
+#         img = np.array(Image.open(img_path))
+#         img = img/255
+#         img = img[np.newaxis, 0:1280, 0:1918, :]
 
-        for i in range(1280):
-            for j in range(1918):
-                if label_result[i, j] > 0.5:
-                    label_result[i, j] = 1
-                else:
-                    label_result[i,j] = 0
+#         model = load_model('models/{}.hdf5'.format(model_name), custom_objects=custom_objects())
+
+#         label_result = model.predict(img)
+#         label_result = np.squeeze(label_result)
+
+#         for i in range(1280):
+#             for j in range(1918):
+#                 if label_result[i, j] > 0.5:
+#                     label_result[i, j] = 1
+#                 else:
+#                     label_result[i,j] = 0
         
-        label_result = label_result.astype(np.uint8)
+#         label_result = label_result.astype(np.uint8)
 
-        write_model.writerow([filename, rle_encode(label_result)])
-        print(filename, 'has completed (',x+1, 'out of', total, ')')
+#         write_model.writerow([filename, rle_encode(label_result)])
+#         print(filename, 'has completed (',x+1, 'out of', num_total, ')')
